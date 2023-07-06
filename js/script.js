6fae6e5614a4a6ba3502de260d75419133fdcc63
Timothy <timsu100@gmail.com>
2023-07-06 00:45:42 -0700

    button function

--- js/script.js
+++ js/script.js
@@ -1,16 +1,30 @@
-var startBtn = document.querySelector("start-btn");
-var submitBtn = document.querySelector("submit-btn");
-var timerEl = document.querySelector("timer");
-var startScreen = document.querySelector("star-screen");
-var quizQame = document.querySelector("quiz-game");
-var quizEnd = document.querySelector("quiz-end");
-var highscoreView = document.querySelector("highscore-view")
+var startBtn = document.querySelector(".start-btn");
+var submitBtn = document.querySelector(".submit-btn");
+var timerEl = document.querySelector(".timer");
+var startScreen = document.querySelector(".start-screen");
+var quizQame = document.querySelector(".quiz-game");
+var quizEnd = document.querySelector(".quiz-end");
+var highscoreView = document.querySelector(".highscore-view");
+var highscoreRedirect = document.querySelector(".highscore-redirect");
+var returnHome = document.querySelector(".return-home");
+var clearScores = document.querySelector(".clear-scores");
 
-
+var quizQuestion = {
+  "question1":["A","B","C","D",1],
+  "question2":["A","B","C","D",3],
+  "question3":["A","B","C","D",0],
+  "question4":["A","B","C","D",2],
+  "question5":["A","B","C","D",1],
+}
 
 //click start button
-startBtn.addEventListener("click",startGame);
+startBtn.addEventListener("click", displayQuiz);
 
+highscoreRedirect.addEventListener("click", displayHighscore);
+
+returnHome.addEventListener("click", displayHome);
+
+clearScores.addEventListener("click", clearHighscore)
 
 //timer starts and question appears
 
@@ -21,8 +35,21 @@ startBtn.addEventListener("click",startGame);
 } */
 
 //startscreen disapears and first question appears
-function displayQuiz(){
-  
+function displayHome(){
+  startScreen.style.display = "flex";
+  highscoreView.style.display = "none";
+}
+
+function displayQuiz() {
+  startScreen.style.display = "none";
+  quizQame.style.display = "flex";
+}
+
+function displayHighscore() {
+  startScreen.style.display = "none";
+  quizQame.style.display = "none";
+  quizEnd.style.display = "none";
+  highscoreView.style.display = "flex";
 }
 //When question is answered correctly
 
@@ -36,3 +63,17 @@ function displayQuiz(){
 
 //When game ends a prompt to save user's initial and highscore appears
 
+
+//clear highscore when button is pressed
+function clearHighscore(){
+
+}
+
+
+
+function init() {
+  quizQame.style.display = "none";
+  quizEnd.style.display = "none";
+  highscoreView.style.display = "none";
+}
+init();
