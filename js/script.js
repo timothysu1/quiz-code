067afa29cd3cec43dbaa893acf80dbc41cf4fa42
Timothy <timsu100@gmail.com>
2023-07-05 23:09:17 -0700

    site structure and js vars

---
+++ js/script.js
@@ -0,0 +1,38 @@
+var startBtn = document.querySelector("start-btn");
+var submitBtn = document.querySelector("submit-btn");
+var timerEl = document.querySelector("timer");
+var startScreen = document.querySelector("star-screen");
+var quizQame = document.querySelector("quiz-game");
+var quizEnd = document.querySelector("quiz-end");
+var highscoreView = document.querySelector("highscore-view")
+
+
+
+//click start button
+startBtn.addEventListener("click",startGame);
+
+
+//timer starts and question appears
+
+/* function startGame (event){
+  if (event.target.matches("button")){
+    
+  }
+} */
+
+//startscreen disapears and first question appears
+function displayQuiz(){
+  
+}
+//When question is answered correctly
+
+//Another question is shown
+
+//When a question is answered incorrectly
+
+//time is removed from timer (~10sec)
+
+//Game ends when all questions are answered or timer reaches 0
+
+//When game ends a prompt to save user's initial and highscore appears
+
